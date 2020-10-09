d43a59def7644bb537568d5f3ac470f72372d387
Paulsonbigi <adegbokanpaul@gmail.com>
2020-10-09 13:56:51 +0100

    Updates

---
+++ precache-manifest.9989dda47e1bc1a5dcb92f64fb7fffb6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16ff356f1051e07263c9b5a9725cfa46",
+    "url": "/instagram-clone/index.html"
+  },
+  {
+    "revision": "5354aa54830fc9f5f11b",
+    "url": "/instagram-clone/static/css/main.24403f65.chunk.css"
+  },
+  {
+    "revision": "5b0fada278d78010b660",
+    "url": "/instagram-clone/static/js/2.6a332577.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/instagram-clone/static/js/2.6a332577.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5354aa54830fc9f5f11b",
+    "url": "/instagram-clone/static/js/main.1f6da3e2.chunk.js"
+  },
+  {
+    "revision": "0e27c518211ad183bf21",
+    "url": "/instagram-clone/static/js/runtime-main.0a269442.js"
+  },
+  {
+    "revision": "5dee8a14dd8514f0294df61afe51eaee",
+    "url": "/instagram-clone/static/media/img1.5dee8a14.jpg"
+  },
+  {
+    "revision": "ecb247571b9198eeeda17c4b86e55419",
+    "url": "/instagram-clone/static/media/img2.ecb24757.jpg"
+  },
+  {
+    "revision": "435ac44023af229f88383a1d27bc4cbe",
+    "url": "/instagram-clone/static/media/img3.435ac440.jpg"
+  }
+]);
